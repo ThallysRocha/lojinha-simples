5800074e2d6ec7314037656d984ed861d055588b
Thallys Christ dos Santos Rocha <tcsr@uhwc08.windows.cin.ufpe.br>
2022-08-11 09:27:29 -0300

    fix: nome da tela inicial

---
+++ src/components/TelaInicial.jsx
@@ -0,0 +1,12 @@
+import TelaDeLogin from './TelaDeLogin';
+import "../styles/TelaInicial.css"
+const TelaInicial = () => {
+    return(
+        <div>
+            <TelaDeLogin/>
+            
+        </div>
+    );
+};
+
+export default TelaInicial
