61b645c80855e21761ac2b0b87fd50182f9dba56
Thallys <tcsr@cin.ufpe.br>
2022-08-11 23:34:05 -0300

    feat: adicionado o botões que faltavam
    Foram adicionados as funções remover item do carrinho e ir para a finalização da compra.

--- src/components/TelaDoCarrinho.jsx
+++ src/components/TelaDoCarrinho.jsx
@@ -26,31 +26,46 @@ const TelaDoCarrinho = () => {
             }
         });
     }
+    const subProduct = (product) =>{
+        products.forEach(item =>{
+            if(item === product){                
+                if(item.quantity>1)item.quantity--;
+                else{
+                    removeProduct(item);
+                }
+                window.localStorage.setItem('cart',JSON.stringify(products));
+                navigate("/carrinho");
+            }
+        });
+    }
     
   return (
     <>
       <h1 className="title">Carrinho</h1>
       
       <div className="productList">
+        <button className="buy" onClick={()=>{navigate("/finalizado")}}>Finalizar compra</button>
         <div className="productList-body">{
           products.map((product) => {
             return (          
                 <div className="product">
+                    <button className="removeProduct" onClick={()=>{removeProduct(product)}}>excluir</button>
                     <div className="pic" ><img src={`${product.image}`} alt={`${product.title}`} /></div>
                     <h3 className="name">{product.title}</h3>
                     <p className="category">{product.category}</p>
                     <h4 className="price">R${(5.27*product.price).toFixed(2)}</h4>
-                    <button className="cartAdd" onClick={()=>{addProduct(product)}}>Adicionar mais um</button>                    
-                    <p>quantidade no carrinho: {product.quantity}</p>
-                    <button className="cartAdd" onClick={()=>{removeProduct(product)}}></button>
+                    <div className="quantity">
+                        <button className="add" onClick={()=>{addProduct(product)}}>adicionar</button>
+                        <button className="remove" onClick={()=>{subProduct(product)}}>remover</button>                     
+                        <p>quantidade no carrinho: {product.quantity}</p>    
+                    </div>
+                                    
+                    
 
                 </div>
             );
           })}
         </div>
-        <div>
-          finalizar compra
-      </div>
       </div>
       
       
